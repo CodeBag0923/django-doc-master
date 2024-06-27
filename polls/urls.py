b383852ffe5bf2afb41c986cbd249e035c878e70
CodeBag0923 <codemaven0923@gmail.com>
2024-06-27 12:36:57 -0700

    complete project

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-  path("", views.index, name="index"),
-  # ex: /polls/5/
-  path("<int:question_id>/", views.detail, name="detail"),
-  # ex: /polls/5/result/
-  path("<int:question_id>/result/", views.detail, name="result"),
-  # ex: /polls/5/vote/
-  path("<int:question_id>/vote/", views.vote, name="vote")
+    path('list/', views.polls_list, name='list'),
+    path('list/user/', views.list_by_user, name='list_by_user'),
+    path('add/', views.polls_add, name='add'),
+    path('edit/<int:poll_id>/', views.polls_edit, name='edit'),
+    path('delete/<int:poll_id>/', views.polls_delete, name='delete_poll'),
+    path('end/<int:poll_id>/', views.end_poll, name='end_poll'),
+    path('edit/<int:poll_id>/choice/add/', views.add_choice, name='add_choice'),
+    path('edit/choice/<int:choice_id>/', views.choice_edit, name='choice_edit'),
+    path('delete/choice/<int:choice_id>/',
+         views.choice_delete, name='choice_delete'),
+    path('<int:poll_id>/', views.poll_detail, name='detail'),
+    path('<int:poll_id>/vote/', views.poll_vote, name='vote'),
 ]

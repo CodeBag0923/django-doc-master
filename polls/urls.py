fbfcc5cb73663ff48fb867b2917c0834d7f5039b
CodeBag0923 <codemaven0923@gmail.com>
2024-06-27 12:03:50 -0700

    add detail page

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   # ex: /polls/5/
   path("<int:question_id>/", views.detail, name="detail"),
   # ex: /polls/5/result/
-  path("<int:question_id>/result/", views.results, name="result"),
+  path("<int:question_id>/result/", views.detail, name="result"),
   # ex: /polls/5/vote/
   path("<int:question_id>/vote/", views.vote, name="vote")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -12,12 +13,10 @@ def index(request):
   return HttpResponse(template.render(context, request))
 
 
-def detail( request, question_id):
-  return HttpResponse("You are looking at question %s" % question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-  response = "You are looking at the result of question %s"
-  return HttpResponse(response % question_id)
 
 def vote(request, question_id):
   return HttpResponse("You are voting on question %s" % question_id)
